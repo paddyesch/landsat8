64aa0b093ffa7194aed9c2331be5e451f57bedff
Patrick Eschenbach <paddyesch@gmx.de>
2016-10-09 22:42:22 +0200

    Collecting metadata

--- landsat8/core.py
+++ landsat8/core.py
@@ -26,6 +26,7 @@ import re
 import os
 import shutil
 import subprocess
+import json
 from landsat.downloader import Downloader
 from landsat.image import Simple
 from colorama import init, Fore
@@ -90,6 +91,80 @@ def convert_scene(scene_id, quality_in_percent, resize_in_percent):
     subprocess.call(command, shell=True)
     print ""
 
+def parse_mtl(mtl_path):
+    mtl = dict()
+    groupStack = [mtl];
+
+    for line in open(mtl_path, "r"):
+        if line.strip() == "END":
+            break
+
+        parts = line.split("=", 1)
+        key   = parts[0].strip()
+        value = parts[1].strip()
+
+        if key == "GROUP":
+            group = dict()
+            groupStack[-1][value] = group
+            groupStack.append(group)
+        elif key == "END_GROUP":
+            groupStack.pop()
+        else:
+            groupStack[-1][key] = value
+
+    return mtl
+
+def determine_country(lat, lon):
+    connection = httplib.HTTPConnection("maps.googleapis.com")
+    connection.request("GET", "/maps/api/geocode/json?latlng={},{}".format(lat, lon))
+    response = json.load(connection.getresponse())
+    if response["status"] == "OK":
+        for result in response["results"]:
+            if "country" in result["types"]:
+                return result["formatted_address"]
+    return ""
+
+def determine_countries(mtl):
+    ll_lat = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LL_LAT_PRODUCT"]
+    ll_lon = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LL_LON_PRODUCT"]
+    lr_lat = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LR_LAT_PRODUCT"]
+    lr_lon = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LR_LON_PRODUCT"]
+    ur_lat = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_UR_LAT_PRODUCT"]
+    ur_lon = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_UR_LON_PRODUCT"]
+    ul_lat = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_UL_LAT_PRODUCT"]
+    ul_lon = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_UL_LON_PRODUCT"]
+
+    countries = set([determine_country(ll_lat, ll_lon),
+                     determine_country(lr_lat, lr_lon),
+                     determine_country(ur_lat, ur_lon),
+                     determine_country(ul_lat, ul_lon)])
+    if "" in countries: countries.remove("")
+    return countries
+
+def collect_metadata(scene_id):
+    print Fore.GREEN + "# Collecting metadata ..."
+    mtl_path = work_folder + "/" + scene_id + "/" + scene_id + "_MTL.txt"
+    mtl = parse_mtl(mtl_path)
+    date = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["DATE_ACQUIRED"]
+    ll_lat = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LL_LAT_PRODUCT"]
+    ll_lon = mtl["L1_METADATA_FILE"]["PRODUCT_METADATA"]["CORNER_LL_LON_PRODUCT"]
+    countries = list(determine_countries(mtl))
+    print "Date acquired: " + date
+    print "Lower left latitude: " + ll_lat
+    print "Lower left longitude: " + ll_lon
+    print "Countries: " + str(countries)
+
+    metadata = {}
+    metadata["date_acquired"] = date
+    metadata["lower_left_lat"] = ll_lat
+    metadata["lower_left_lon"] = ll_lon
+    metadata["countries"] = countries
+
+    metadataPath = scene_id + "_metadata.json"
+    with open(metadataPath, "w") as outfile:
+        json.dump(metadata, outfile)
+    print ""
+
 def clean_up_scene(scene_id):
     print Fore.GREEN + "# Cleaning up ..."
     clean_path = work_folder + "/" + scene_id
@@ -105,4 +180,5 @@ def clean_up_scene(scene_id):
 def print_scene_result(scene_id):
     print Fore.GREEN + "# Scene image created ..."
     print scene_id + ".JPG"
+    print scene_id + "_metadata.json"
     print ""
